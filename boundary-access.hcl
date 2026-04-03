c92d4e72034f857ca07223de5d6a1cf5b559b27b
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2026-04-03 15:09:25 -0400

    initial

---
+++ boundary-access.hcl
@@ -0,0 +1,9 @@
+
+path "sandbox-ssh-user-ca/issue/*" {
+	capabilities = ["read"]
+}
+
+path "sandbox-ssh-user-ca/sign/*" {
+	capabilities = ["read"]
+}
+
---
+++ main.tf
@@ -0,0 +1,37 @@
+
+resource "vault_identity_entity" "boundary" {
+  name      = "HCP Boundary"
+}
+
+resource "vault_policy" "boundary-controller" {
+	name      = "boundary-controller"
+	policy    = file("${path.module}/boundary-controller.hcl")
+}
+
+resource "vault_policy" "secrets" {
+	name      = "boundary-access"
+	policy    = file("${path.module}/boundary-access.hcl")
+}
+
+resource "vault_token" "boundary" {
+  role_name = vault_token_auth_backend_role.boundary.role_name
+  no_parent = true
+  renewable = true
+  period = "24h"
+}
+
+resource "vault_token_auth_backend_role" "boundary" {
+  role_name              = "credential-store"
+  allowed_policies = [vault_policy.boundary-controller.name, vault_policy.secrets.name]
+  disallowed_policies    = ["default"]
+  allowed_entity_aliases = [vault_identity_entity.boundary.name]
+  orphan                 = true
+  token_period           = "86400"
+  renewable              = true
+}
+
+output "token" {
+	value     = vault_token.boundary.client_token
+	sensitive = true
+}
+
